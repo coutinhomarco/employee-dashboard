d572511db2de202d64adcb088114b95904ccb590
coutinhomarco <marquinco@outlook.com>
2024-05-16 17:16:32 -0300

    create: employee model

---
+++ server/src/models/employeeModel.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const employeeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  position: { type: String, required: true },
+  department: { type: String, required: true },
+  dateOfHire: { type: Date, required: true }
+});
+
+const Employee = mongoose.model('Employee', employeeSchema);
+
+export default Employee;
